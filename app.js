60a66007d680abde2ac479e2a36b8c1dbdaf5d67
tahahajjout <taha2000hajjout@gmail.com>
2026-04-20 17:05:18 +0200

    "database connection

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+const dns = require('node:dns');
+dns.setDefaultResultOrder('ipv4first');
+
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const app = express()
 const port = 3004
-
+const mongose = require('mongoose')
+const dbURI = "mongodb://taha:taha2005@ac-rzfmx2o-shard-00-00.us4e7rc.mongodb.net:27017,ac-rzfmx2o-shard-00-01.us4e7rc.mongodb.net:27017,ac-rzfmx2o-shard-00-02.us4e7rc.mongodb.net:27017/?ssl=true&replicaSet=atlas-a1xj3p-shard-0&authSource=admin&appName=Cluster0";
 app.get('/', (req, res) => {
   res.sendFile("./views/home.html",{root:__dirname})
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
+mongose.connect(dbURI).then(()=>{
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
 })
+.catch((err)=>{console.log(err)
+
+});
