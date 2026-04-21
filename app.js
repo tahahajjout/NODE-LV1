5ca3b0f29e2cad4385a65309d38be555706e831c
tahahajjout <taha2000hajjout@gmail.com>
2026-04-21 18:52:18 +0200

    11

--- app.js
+++ app.js
@@ -9,10 +9,20 @@ const mongose = require('mongoose')
 const dbURI = "mongodb://taha:taha2005@ac-rzfmx2o-shard-00-00.us4e7rc.mongodb.net:27017,ac-rzfmx2o-shard-00-01.us4e7rc.mongodb.net:27017,ac-rzfmx2o-shard-00-02.us4e7rc.mongodb.net:27017/all-data?ssl=true&replicaSet=atlas-a1xj3p-shard-0&authSource=admin&appName=Cluster0";
 app.use(express.urlencoded({extended:true}));
 const Mydata = require("./models/mydataSchema");
+const { rmSync } = require('node:fs');
+app.set('view engine','ejs')
 
 app.get('/', (req, res) => {
-  res.sendFile("./views/home.html",{root:__dirname})
-})
+ /* Mydata.find()
+  .then((result) => {
+    res.render("home",{mytitle:"Home page" , arr: result});
+  })
+  .catch((err) => {
+    console.log(err);
+  });*/
+  res.render("home", { mytitle: "Home page", mydata: null });
+
+});
 
 mongose.connect(dbURI).then(()=>{
     console.log("✅ Connected to MongoDB Atlas!"),
@@ -27,6 +37,5 @@ mongose.connect(dbURI).then(()=>{
 app.post("/",(req,res) => {
   console.log(req.body)
   const mydata = new Mydata(req.body);
-  mydata.save()
-  res.redirect("/index.html")
+  mydata.save().then((result) =>res.render("home", { mytitle: "Home page", mydata: result }));
 });
